9021e8f2be12c1dcef109d1216bd6dc69a0e1548
AardvarksUpInYoFace <jonathanjacob_L@hotmail.com>
2016-11-22 14:46:06 +0000

    Slight changes to fix up class architecture
    removal of obsolete classes and methods

--- core/src/com/jormandr/Game/Game.java
+++
@@ -1,5 +0,0 @@
-package com.jormandr.Game;
-
-public class Game {
-
-}
--- core/src/com/jormandr/gameobjects/Plot.java
+++ core/src/com/jormandr/gameobjects/Plot.java
@@ -1,43 +1,12 @@
 package com.jormandr.gameobjects;
 
 public class Plot extends MapTile {
-	private int resources[] = new int[3];
-	private int ownership;
-	private boolean enabled;
-	// Todo: Create accessors/mutators
 
-	public Plot(int sres[]) {
-		resources[0] = sres[0];
-		resources[1] = sres[1];
-		resources[2] = sres[2];
-		ownership = 0;
-		enabled = false;
-	}
-
-	public void interact(int player) {
-		if (player == ownership) {
-			if (enabled) {
-				upgradeMenu();
-			} else {
-				installMenu();
-			}
-		} else if (ownership == 0) {
-			PurchaseMenu();
-		}
-	}
-
-	private void PurchaseMenu() {
-		// TODO Auto-generated method stub
+	public Plot() {
+		// in here, figure out what type tile I am, what resource values I have, where I am located
+		
 		
 	}
-
-	private void installMenu() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void upgradeMenu() {
-		// TODO Auto-generated method stub
-		
-	}
+	
+	
 }
