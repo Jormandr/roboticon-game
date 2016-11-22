7afd46c975bdb345b71720ec686e066bb461b15e
Mark Henrick <mshenrick@users.noreply.github.com>
2016-11-22 13:24:49 +0000

    Fix Plot class

--- core/src/com/jormandr/gameobjects/Plot.java
+++ core/src/com/jormandr/gameobjects/Plot.java
@@ -1,27 +1,42 @@
-package GameObject;
+package com.jormandr.gameobjects;
 
-public class Plot Extends MapTile {
-int resources[]=new int[3];
-int ownership;
-boolean enabled;
-public Plot(int sres[]){
-  resources[0]=sres[0];
-  resources[1]=sres[1];
-  resources[2]=sres[2];
-  ownership=0;
-  enabled=false;
-}
-public void interact(int player){
-  if(player==ownership){
-    if(enabled){
-    upgradeMenu();
-    }
-    else{
-    installMenu();
-    }
-  }
-  else if(ownership==0){
-  PurchaseMenu();
-  }
-}
+public class Plot extends MapTile {
+	int resources[] = new int[3];
+	int ownership;
+	boolean enabled;
+
+	public Plot(int sres[]) {
+		resources[0] = sres[0];
+		resources[1] = sres[1];
+		resources[2] = sres[2];
+		ownership = 0;
+		enabled = false;
+	}
+
+	public void interact(int player) {
+		if (player == ownership) {
+			if (enabled) {
+				upgradeMenu();
+			} else {
+				installMenu();
+			}
+		} else if (ownership == 0) {
+			PurchaseMenu();
+		}
+	}
+
+	private void PurchaseMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void installMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void upgradeMenu() {
+		// TODO Auto-generated method stub
+		
+	}
 }
