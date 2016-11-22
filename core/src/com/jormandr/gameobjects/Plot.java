e829ac0fc984d79ebebd085ae73514e6c29cde08
jt1174 <jt1174@york.ac.uk>
2016-11-22 13:08:44 +0000

    Plot class
    buy,install and upgrade menu method TBD

---
+++ core/src/com/jormandr/gameobjects/Plot.java
@@ -0,0 +1,27 @@
+package GameObject;
+
+public class Plot Extends MapTile {
+int resources[]=new int[3];
+int ownership;
+boolean enabled;
+public Plot(int sres[]){
+  resources[0]=sres[0];
+  resources[1]=sres[1];
+  resources[2]=sres[2];
+  ownership=0;
+  enabled=false;
+}
+public void interact(int player){
+  if(player==ownership){
+    if(enabled){
+    upgradeMenu();
+    }
+    else{
+    installMenu();
+    }
+  }
+  else if(ownership==0){
+  PurchaseMenu();
+  }
+}
+}
