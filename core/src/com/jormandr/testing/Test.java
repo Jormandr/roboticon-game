adbaba889c49255053913ae180a169958f27f78e
jt1174 <jt1174@york.ac.uk>
2016-11-25 16:08:14 +0000

    documentation added

--- core/src/com/jormandr/testing/Test.java
+++ core/src/com/jormandr/testing/Test.java
@@ -12,27 +12,40 @@ class noexception extends Exception{
 }
 public class Test {
 	//Test class, tests for individual functions denoted as test_fnname
+	//log is refreshed each time, to store permanently, add fn or save screen
 String log[]=new String[100];
 private int count[]=new int[100];
 String keys[]=new String[100];
 private int maxint=Integer.MAX_VALUE;
-private int limit=0;
+//private int limit=0;
 public enum testfn{
 	//testfunction names
 	TEST
 }
 void intvaltest(){
+	//tests if the machine's max integer value is greater than the counter limit
+	System.out.println("integer limit");
 	if(999999>maxint){
 		System.out.println("fail:int error");
 	}
+	else{
+		System.out.println("pass: max value "+maxint);
+	}
 }
+/*
+ //stores detailed error report
+void debug_mode(testfn fn){
+}*/
 void looptest(testfn fn){
+	//looptests the function given by the enum 100 times
+	System.out.println("looptest run");
 	//selects function to loop
 	for(int i=0; i<100; i++){
 	try{
 		switch(fn){
 		//cases for each test function
 		case TEST:{
+			//test case for testing the test function
 			if (i%4==0){			
 			throw new NullPointerException();
 			}
@@ -45,6 +58,7 @@ void looptest(testfn fn){
 					
 		}
 		}
+		//stores each iteration to the log array
 		log[i]="ok";
 	}
 	catch(Exception e){
@@ -53,6 +67,9 @@ void looptest(testfn fn){
 }
 }
 int[] errorfrequency(){
+	//groups the log by error type and prints the frequency of occurence
+	//format: times_occured		errorname(ok means no error)
+	System.out.println("frequency-type report");
 	int j;
 	for(int i=0;i<100;i++){
 		
