4ed9e3352401cda13cfa2d97286610330486e714
jt1174 <jt1174@york.ac.uk>
2016-11-25 15:41:39 +0000

    Testing frame with test of testing frame, statistics: type-total 100 looptesting

---
+++ core/src/com/jormandr/testing/Test.java
@@ -0,0 +1,83 @@
+package com.jormandr.testing;
+
+import java.util.EmptyStackException;
+
+class noexception extends Exception{
+	//no exception found
+	private static final long serialVersionUID = 1L;
+
+	noexception(String s){
+		super(s);
+	}
+}
+public class Test {
+	//Test class, tests for individual functions denoted as test_fnname
+String log[]=new String[100];
+private int count[]=new int[100];
+String keys[]=new String[100];
+private int maxint=Integer.MAX_VALUE;
+private int limit=0;
+public enum testfn{
+	//testfunction names
+	TEST
+}
+void looptest(testfn fn){
+	//selects function to loop
+	for(int i=0; i<100; i++){
+	try{
+		switch(fn){
+		//cases for each test function
+		case TEST:{
+			if (i%4==0){			
+			throw new NullPointerException();
+			}
+			else if (i%4==1){
+			throw new EmptyStackException();
+			}
+			else if (i%4==2){
+			throw new ClassCastException();
+			}
+					
+		}
+		}
+		log[i]="ok";
+	}
+	catch(Exception e){
+		log[i]=e.toString();
+	}
+}
+}
+int[] errorfrequency(){
+	int j;
+	for(int i=0;i<100;i++){
+		
+		for(j=0;j<100;j++){
+			if(keys[j]==null){
+				keys[j]=log[i];
+				break;
+			}
+			else if(keys[j].equals(log[i])){
+				break;
+			}
+		}
+	count[j]++;	
+	}
+	for(int i=0;i<100;i++){
+		if(keys[i]==null){
+			break;
+		}
+	System.out.println(count[i]+"      "+keys[i]);
+	}
+	return count;
+}
+
+int median(){
+	return 0;
+}
+int mode(){
+	return 0;
+}
+void alltest(){
+	//run all tests once
+}
+}
---
+++ core/src/com/jormandr/testing/TestRun.java
@@ -0,0 +1,13 @@
+package com.jormandr.testing;
+import com.jormandr.testing.Test;
+public class TestRun {
+	
+	public static void main(String[] args) {
+		int x[];
+		Test t1=new Test();
+		// TODO Auto-generated method stub
+		t1.looptest(Test.testfn.TEST);
+		x=t1.errorfrequency();
+	}
+
+}
