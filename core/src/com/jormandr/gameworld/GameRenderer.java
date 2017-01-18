43e8a3821dddc308c6745c79f6dbfd94199a8cc9
AardvarksUpInYoFace <jonathanjacob_L@hotmail.com>
2017-01-18 09:46:39 +0000

    Reformat of two conflicted classes
    Just a format of the code in gamerenderer and assetloader

--- core/src/com/jormandr/gameworld/GameRenderer.java
+++ core/src/com/jormandr/gameworld/GameRenderer.java
@@ -7,13 +7,11 @@ import com.jormandr.gameobjects.Plot;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.jormandr.helpers.AssetLoader;
 import com.jormandr.helpers.CollisionHandler;
-import com.jormandr.helpers.GameStateHandler;
 
 /**
  * This is the class that deals with all rendering of sprites and any other
@@ -117,50 +115,43 @@ public class GameRenderer {
 
 		AssetLoader shit = new AssetLoader(); // TODO remember why we called
 												// this 'shit' then fix
-		
-		//doesn't work 100% of the time
 
+		// doesn't work 100% of the time
 
 		// drawing background
 		batcher.draw(AssetLoader.backgroundTexture, 0, 0);
 
-		
 		// drawing the map grid
 		for (int i = 0; i < arrayX; i++) {
 			for (int j = 0; j < arrayY; j++) {
-				
+
 				MapTile tile = worldMap[i][j];
 				float xx = tile.convertToX();
 				float yy = tile.convertToY();
-				
-				if (tile == CollisionHandler.getNearestMapTile()
-						&& CollisionHandler.tileMouseOver() == true) {
-					batcher.draw(AssetLoader.textureMap[tile.getType().ordinal()], xx,
-							yy + 60, 124, -68);
-					
-					if (myWorld.getGameState() == 0){
 
-						
-						batcher.setColor(1.0f, 0.5f, 0.5f, 1.0f); 
-						batcher.draw(AssetLoader.uiTileInfo,xx+64, yy, 0, 0, 20, -28, 4, 4, 0);
-						batcher.setColor(1.0f, 1.0f, 1.0f, 1.0f); 
-						AssetLoader.fontX.draw(batcher, tile.getType().toString(), xx+76, yy - 100);
-				
-						if (tile instanceof Plot){
-						AssetLoader.fontX.draw(batcher, "Food: " + ((Plot) tile).getFoodValue(), xx+76, yy - 80);
-						AssetLoader.fontX.draw(batcher, "Ore: " + ((Plot) tile).getOreValue(), xx+76, yy - 65);
+				if (tile == CollisionHandler.getNearestMapTile() && CollisionHandler.tileMouseOver() == true) {
+					batcher.draw(AssetLoader.textureMap[tile.getType().ordinal()], xx, yy + 60, 124, -68);
+
+					if (myWorld.getGameState() == 0) {
+
+						batcher.setColor(1.0f, 0.5f, 0.5f, 1.0f);
+						batcher.draw(AssetLoader.uiTileInfo, xx + 64, yy, 0, 0, 20, -28, 4, 4, 0);
+						batcher.setColor(1.0f, 1.0f, 1.0f, 1.0f);
+						AssetLoader.fontX.draw(batcher, tile.getType().toString(), xx + 76, yy - 100);
+
+						if (tile instanceof Plot) {
+							AssetLoader.fontX.draw(batcher, "Food: " + ((Plot) tile).getFoodValue(), xx + 76, yy - 80);
+							AssetLoader.fontX.draw(batcher, "Ore: " + ((Plot) tile).getOreValue(), xx + 76, yy - 65);
 							AssetLoader.fontX.draw(batcher, "Energy: " + ((Plot) tile).getEnergyValue(), xx + 76,
 									yy - 50);
 						}
 					}
-					
+
 				} else {
-					batcher.draw(AssetLoader.textureMap[tile.getType().ordinal()], xx,
-							yy + 64, 124, -68);
+					batcher.draw(AssetLoader.textureMap[tile.getType().ordinal()], xx, yy + 64, 124, -68);
 				}
 			}
 		}
-
 
 		// drawing the UI
 		batcher.draw(AssetLoader.uiBottom, 0, 720, 0, 0, 320, -51, 4, 4, 0);
@@ -175,7 +166,6 @@ public class GameRenderer {
 				batcher.draw(AssetLoader.uiStateLightOff, 640 - 128 + k * 32, 81 + 28, 0, 0, 7, -7, 4, 4, 0);
 			}
 		}
-
 
 		batcher.disableBlending();
 
